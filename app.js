786bbfbba5ad9e61730357da33d4a3c099ed07ea
MindInSky <kevin.a.muniz@gmail.com>
2018-09-19 10:36:53 -0500

    js added extends

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 class Dog {
   constructor(name, breed, dob) {
     this.name = name;
@@ -30,4 +31,52 @@ class Person {
   static sum(x, y) {
     return this.x + this.y;
   }
+  static yearsInBetween(date1, date2) {
+    const iniDate = new Date(date1);
+    const endDate = new Date(date2);
+    const diff = endDate.getTime() - iniDate.getTime();
+    const epoch = new Date(diff);
+    return epoch.getFullYear() - 1970;
+  }
 }
+
+
+
+const hilary = new Person("Hilary", "Rodham", "26/Oct/1947");
+*/
+
+class Fruit {
+  constructor(name, color, calories) {
+    this.name = name;
+    this.color = color;
+    this.calories = calories;
+  }
+
+  info() {
+    return `this ${this.color} ${this.name} contains ${
+      this.calories
+    } calories of raw energy!`;
+  }
+}
+
+class Smoothie extends Fruit {
+  constructor(name, color, calories, size, hasMilk) {
+    super(name, color, calories);
+
+    this.size = size;
+    this.hasMilk = hasMilk;
+  }
+  countCalories() {
+    let totalCals;
+    if (this.size === "grande") {
+      totalCals = Number.parseInt(this.calories) * 3;
+    } else if (this.size === "mediano") {
+      totalCals = Number.parseInt(this.calories);
+    } else {
+      totalCals = this.calories + 20;
+    }
+    return totalCals;
+  }
+}
+
+bananaSmoothie = new Smoothie("platano", "amarillo", 89, "grande", true);
